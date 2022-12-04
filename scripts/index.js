3cb485c07dbaf0b3bbe47f26fb934facdacb3d92
Polina Khvorostukhina <polinakhvorost@yandex.ru>
2022-12-04 15:00:13 +0300

    feat: создать массив карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+
+// Прописываем алгоритм редактирования данных профиля
 // Объявляем переменные
 const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
@@ -39,3 +41,62 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+// Создаем массив карточек
+const cardGridArray = [
+  {
+    source: './images/card-grid/altai.png',
+    description: 'Алтай. Горное озеро.Каменный склон. Лес на берегу.',
+    name: 'Алтай'
+  },
+  {
+    source: './images/card-grid/baikal.png',
+    description: 'Байкал. Остров Огой.Заледеневшее озеро. Скала.',
+    name: 'Байкал'
+  },
+  {
+    source: './images/card-grid/elbrus.png',
+    description: 'Эльбрус. Вершины. Горный массив. Река в долине.',
+    name: 'Эльбрус'
+  },
+  {
+    source: './images/card-grid/kamchatka.png',
+    description: 'Камчатка. Вулкан в облаках. Черный склон со снегом.',
+    name: 'Камчатка'
+  },
+  {
+    source: './images/card-grid/karelia.png',
+    description: 'Карелия. Скалистый берег реки. Сосновый лес.',
+    name: 'Карелия'
+  },
+  {
+    source: './images/card-grid/sochi.png',
+    description: 'Сочи. Горный массив в зелени. Серпантин. Туман.',
+    name: 'Сочи'
+  },
+]
+
+const cardGridElement = document.querySelector('.card-grid__container');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card-grid__item');
+
+function createElement(item) {
+
+  const card = cardTemplate.cloneNode(true);
+
+  const cardImage = card.querySelector('.card-grid__image');
+  const cardName = card.querySelector('.card-grid__name');
+
+  cardImage.setAttribute('src', item.source);
+  cardImage.setAttribute('alt', item.description);
+  cardName.textContent = item.name;
+
+  return card
+
+}
+
+cardGridArray.forEach(function(item) {
+  const element = createElement(item)
+  cardGridElement.append(element);
+}) 
