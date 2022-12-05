6f329d87cab790eb7b7bf2e35929e7b5e163a4d2
Polina Khvorostukhina <polinakhvorost@yandex.ru>
2022-12-05 10:44:57 +0300

    feat: добавлять и удалять катрочки мест, лайкать карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,34 @@
-
 // Прописываем алгоритм редактирования данных профиля
-// Объявляем переменные
-const popupElement = document.querySelector('.popup');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 
-const formElement = document.querySelector('.popup__content');
-const nameInput = formElement.querySelector('.popup__input_type_name');
-const jobInput = formElement.querySelector('.popup__input_type_job');
+// Объявляем переменные
+const popupProfileInfoElement = document.querySelector('.popup_type_profile-information');
+const popupProfileInfoOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupProfileInfoCloseButtonElement = popupProfileInfoElement.querySelector('.popup__close-button_type_profile-information');
+
+const formProfileInfoElement = popupProfileInfoElement.querySelector('.popup__content');
+const nameInput = formProfileInfoElement.querySelector('.popup__input_type_name');
+const jobInput = formProfileInfoElement.querySelector('.popup__input_type_job');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
 // Подключаем открывание и закрывание popup
-const openPopup = function() {
-  popupElement.classList.add('popup_opened');
+const openPopupProfileInfo = function() {
+  popupProfileInfoElement.classList.add('popup_opened');
 
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-}
+};
 
-const closePopup = function() {
-  popupElement.classList.remove('popup_opened')
-}
+const closePopupProfileInfo = function() {
+  popupProfileInfoElement.classList.remove('popup_opened')
+};
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+popupProfileInfoOpenButtonElement.addEventListener('click', openPopupProfileInfo);
+popupProfileInfoCloseButtonElement.addEventListener('click', closePopupProfileInfo);
 
 // Подключаем редактирование данных профиля через поля ввода в окне popup
-function formSubmitHandler (evt) {
+function formProfileInfoSubmitHandler (evt) {
   evt.preventDefault();
   
   let newNameInput = nameInput.value;
@@ -37,14 +37,67 @@ function formSubmitHandler (evt) {
   profileName.textContent = newNameInput;
   profileJob.textContent = newJobInput;
 
-  closePopup()
+  closePopupProfileInfo()
+};
+
+formProfileInfoElement.addEventListener('submit', formProfileInfoSubmitHandler);
+
+
+
+
+
+// Прописываем алгоритм добавления карточек
+
+// Объявляем переменные
+const popupPlaceAddElement = document.querySelector('.popup_type_place-addition');
+const popupPlaceAddOpenButtonElement = document.querySelector('.profile__add-button');
+const popupPlaceAddCloseButtonElement = popupPlaceAddElement.querySelector('.popup__close-button_type_place-addition');
+
+const formPlaceAddElement = popupPlaceAddElement.querySelector('.popup__content');
+const placeInput = popupPlaceAddElement.querySelector('.popup__input_type_place');
+const linkInput = popupPlaceAddElement.querySelector('.popup__input_type_link');
+
+// Подключаем открывание и закрывание popup
+const openPopupPlaceAdd = function() {
+  popupPlaceAddElement.classList.add('popup_opened');
+
+  placeInput.value = '';
+  linkInput.value = '';
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+const closePopupPlaceAdd = function() {
+  popupPlaceAddElement.classList.remove('popup_opened')
+}
+
+popupPlaceAddOpenButtonElement.addEventListener('click', openPopupPlaceAdd);
+popupPlaceAddCloseButtonElement.addEventListener('click', closePopupPlaceAdd);
+
+// Создаем новые карточки
+function formPlaceAddSubmitHandler (evt) {
+  evt.preventDefault();
+  
+  let newPlaceInput = placeInput.value;
+  let newLinkInput = linkInput.value;
+
+  const newCard = {
+    source: newLinkInput,
+    description: 'Здесь должна быть красивая картинка, но что-то пошло не так. Проверьте правильность написания ссылки на картинку.',
+    name: newPlaceInput
+  };
+
+  const element = createElement(newCard);
+  cardGridElement.prepend(element);
+
+  closePopupPlaceAdd()
+}
+
+formPlaceAddElement.addEventListener('submit', formPlaceAddSubmitHandler);
 
 
 
-// Создаем массив карточек
+
+
+// Создаем массив карточек мест
 const cardGridArray = [
   {
     source: './images/card-grid/altai.png',
@@ -75,9 +128,10 @@ const cardGridArray = [
     source: './images/card-grid/sochi.png',
     description: 'Сочи. Горный массив в зелени. Серпантин. Туман.',
     name: 'Сочи'
-  },
+  }
 ]
 
+// Прописываем алгоритм передачи данных из массива в карточки
 const cardGridElement = document.querySelector('.card-grid__container');
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.card-grid__item');
 
@@ -88,15 +142,30 @@ function createElement(item) {
   const cardImage = card.querySelector('.card-grid__image');
   const cardName = card.querySelector('.card-grid__name');
 
+  const likeButton = card.querySelector('.card-grid__like-button');
+  likeButton.addEventListener('click', handleLikeButtonClick);
+
+  const deleteBytton = card.querySelector('.card-grid__delete-button');
+  deleteBytton.addEventListener('click', handleDeleteButtonClick);
+
   cardImage.setAttribute('src', item.source);
   cardImage.setAttribute('alt', item.description);
   cardName.textContent = item.name;
 
   return card
-
-}
+};
 
 cardGridArray.forEach(function(item) {
   const element = createElement(item)
   cardGridElement.append(element);
-}) 
+});
+
+// Прописываем алгоритм лайка карточек
+function handleLikeButtonClick (evt) {
+  evt.target.classList.toggle('card-grid__like-button_active')
+};
+
+// Прописываем алгоритм удаления карточек
+function handleDeleteButtonClick (evt) {
+  evt.target.closest('.card-grid__item').remove()
+}
